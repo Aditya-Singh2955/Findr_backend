15506147a2db72be13a602774797179e2217a90b
ADITYA <91845934+Aditya-Singh2955@users.noreply.github.com>
2025-09-10 22:09:10 +0530

    Update index.js
    cors issue

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+//   credentials: true
+// }));
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  credentials: true
+  origin: "*",
+  credentials: false
 }));
+
 app.use(express.json());
 
 require("./config/database").connect();
